17712db86f49057e8d78770a5659b6d04d72a206
preetrahul01 <preetikumarioct1998@gmail.com>
2024-10-02 15:14:46 +0530

    javascript

---
+++ first.js
@@ -0,0 +1,19 @@
+//console.log("holle word");
+/*
+fullname ="preeti";
+age = 24;
+price = 99.99;
+x = null;
+y = undefined;
+isfollow = false;
+console.log(fullname);
+*/
+
+
+//defined (let,var,const)
+
+let fullname ="preeti";
+let age = 24;
+let price = 2000;
+
+console.log(fullname);
